9e066bec52708e5c599f23dfba19dd3a4062bf58
Kiwifuit <75169058+Kiwifuit@users.noreply.github.com>
2026-03-31 10:53:16 +0800

    feat: made bootstrap script a bit more robust

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SCRIPT_PATH=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+cd $SCRIPT_DIR || exit 1
 mkdir -p logs
 
 exec python frontend/ivan_is_gay.py >> logs/frontend.log 2>&1
